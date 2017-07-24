ef99bae612fbf81900d91cae254aa819fb12e79e
Ufuoma-Daniel <Ufuoma-Daniel@hotmail.co.uk>
2017-07-24 14:27:49 +0100

    First Commit

---
+++ app/controllers/Application.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+class Application extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+
+  def home = Action {
+    Ok(views.html.home("This is my application."))
+  }
+
+}
