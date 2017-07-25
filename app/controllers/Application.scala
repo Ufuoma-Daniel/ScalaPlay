766dd8c36e337410f70a6cdffa10dfdfb4ee0b68
Ufuoma-Daniel <Ufuoma-Daniel@hotmail.co.uk>
2017-07-25 11:15:20 +0100

    Dev - day2, Sessions, Cookies and Flash

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,12 +6,35 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Your new application is ready.")).discardingCookies(DiscardingCookie("Chocoholic"))
   }
 
   def home = Action {
-    Ok(views.html.home("This is my application."))
+    Ok(views.html.home("This is my application.")).withCookies(Cookie("Chocoholic", "bountysucks"))
   }
+
+  def misc = Action{ implicit request =>
+    Ok(views.html.misc("Hello Daniel")(request)).withSession("connected" -> "danieldoe@fakemail.com")
+  }
+
+  def flash = Action {
+    Redirect("/misc").flashing(
+      "congrats" -> "Flash made")
+  }
+
+
+  def cookieBoy() = Action {
+    Ok("Hello John").withSession("connected" -> "johndoe@fakemail.com")
+  }
+
+  def cookieBoyUpdate() = Action{ implicit request =>
+    Ok("Hello Jane").withSession(request.session + ("connected" -> "janedoe@fakemail.com"))
+  }
+
+  def deleteAllCookies() = Action{ request =>
+    //Ok(request.session.data.foreach(item => request.session - item._1))
+    Ok("DeleteAll").withNewSession
+}
 
   def numbers(num: Any) = Action {
     Ok("The number is "+num)
@@ -28,6 +51,10 @@ class Application extends Controller {
 
   def redirectHome() = Action {
     Redirect("/home")
+  }
+
+  def reverse() = Action {
+    Redirect(routes.Application.test("yeah"))
   }
 
   def incomplete() = TODO
