3605fd5577679ea657e7c3f8e4a987b52d5d272d
Ufuoma-Daniel <Ufuoma-Daniel@hotmail.co.uk>
2017-08-01 16:22:34 +0100

    Dev - Serve and Sending files

--- app/controllers/website.scala
+++ app/controllers/website.scala
@@ -1,9 +1,14 @@
 package controllers
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectOutputStream}
 import javax.inject.Inject
 
+import play.api.http.HttpEntity
 import models.{Item, ItemDel}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
+import HttpEntity.Streamed
+import akka.stream.scaladsl.{FileIO, Source, StreamConverters}
+import akka.util.ByteString
 
 import scala.concurrent.Future
 
@@ -47,6 +52,46 @@ class Website @Inject()(val messagesApi: MessagesApi) extends Controller with I1
       Item.items.foreach(thing =>  if(thing.name.equalsIgnoreCase(item.name)){Item.items -= thing})
       Redirect(routes.Website.listItems())
     })
+  }
+
+  def sendWithLength :Action[AnyContent] = Action {
+    val file = new java.io.File("C:\\Users\\Administrator\\Desktop\\AllWork\\SPlay\\day3send.pdf")
+    val path: java.nio.file.Path = file.toPath
+    val source: Source[ByteString, _] = FileIO.fromPath(path)
+    val contentLength = Some(file.length())
+
+    Result(
+      header = ResponseHeader(200, Map.empty),
+      body = HttpEntity.Streamed(source, contentLength, Some("application/pdf"))
+    )
+  }
+
+  def serveChangedName :Action[AnyContent] = Action {
+    Ok.sendFile(
+      content = new java.io.File("C:\\Users\\Administrator\\Desktop\\AllWork\\SPlay\\day3serve.pdf"),
+      fileName = _ => "renamed.pdf"
+    )
+  }
+
+//  def chunked :Action[AnyContent] = Action {
+//    val data = List("1","2","3","4","5","1","2","3","4","5","1","2","3","4","5")
+//    val dataContent: Source[ByteString, _] = StreamConverters.fromInputStream(() => data)
+//    Ok.chunked(dataContent)
+//  }
+
+
+  def sendChunks :Action[AnyContent] = Action {
+    val data =  new ByteArrayInputStream (serialise("Hello"))
+    val dataContent: Source[ByteString, _] = StreamConverters.fromInputStream(() => data)
+    Ok.chunked(dataContent)
+  }
+
+  def serialise(value: Any): Array[Byte] = {
+    val stream: ByteArrayOutputStream = new ByteArrayOutputStream()
+    val oos = new ObjectOutputStream(stream)
+    oos.writeObject(value)
+    oos.close
+    stream.toByteArray
   }
 
 }
